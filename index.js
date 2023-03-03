5b1c38244dd8e36031645f80048cc953569ac6e7
Sergey Dolin <dsame@github.com>
2023-03-03 03:55:52 +0100

    run

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const github = require('@actions/github');
 
 try {
   const defInput = core.getInput('def-input');
-  console.log(`defInput: ${defInput.slice(0,3)}_${defInput.slice(3)}!`);
   core.setOutput("defOutput", defInput);
   console.log(`DefOutput ${defInput}!`);
   core.exportVariable('DefOutput', defInput);
