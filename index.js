de54b9d5e47638bf42e2252e262f45bf8878ac29
Sergey Dolin <dsame@github.com>
2023-03-03 03:50:06 +0100

    run

--- index.js
+++ index.js
@@ -2,19 +2,12 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
   console.log(process.env);
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-
-  const sampleVar = core.getInput('sample-env-var');
-  console.log(`SampleVar ${sampleVar}!`);
-  core.exportVariable('sampleVar', sampleVar);
+  const defInput = core.getInput('def-input');
+  console.log(`defInput: ${defInput}!`);
+  core.setOutput("defOutput", defInput);
+  console.log(`DefOutput ${defInput}!`);
+  core.exportVariable('DefOutput', defInput);
 
 } catch (error) {
   core.setFailed(error.message);
